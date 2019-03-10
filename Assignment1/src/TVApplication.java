00097693e1825ec6e9aa4e3400abfecc33c4331a
Derpish1 <connor.tehhall@hotmail.co.nz>
2019-03-11 10:19:52 +1300

    init

---
+++ Assignment1/src/TVApplication.java
@@ -0,0 +1,12 @@
+package assignment;
+
+public class TVApplication {
+
+	public static void main(String[] args) 
+	{
+		Television tv = new Television();
+		
+		System.out.println(tv.toString(true, 5, 0));
+	}
+
+}
