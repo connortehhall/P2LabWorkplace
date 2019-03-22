da571b983974eb2433a9eb79b10ec38239762355
Derpish1 <connor.tehhall@hotmail.co.nz>
2019-03-22 17:27:27 +1300

    Lab 4 done

--- Week04/src/exercise6/MultiSphere.java
+++ Week04/src/exercise6/MultiSphere.java
@@ -2,9 +2,17 @@ package exercise6;
 
 public class MultiSphere {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		Sphere sphere1 = new Sphere(5);
+		Sphere sphere2 = new Sphere(10);
+		Sphere sphere3 = new Sphere(12.5);
+		Sphere sphere4 = new Sphere(1.2);
+		
+		System.out.println(sphere1.toString());
+		System.out.println(sphere2.toString());
+		System.out.println(sphere3.toString());
+		System.out.println(sphere4.toString());
 	}
 
 }
--- Week04/src/exercise6/Sphere.java
+++ Week04/src/exercise6/Sphere.java
@@ -2,4 +2,40 @@ package exercise6;
 
 public class Sphere {
 
+	private double diameter;
+	
+	public Sphere(double diameter)
+	{
+		this.setDiameter(diameter);
+	}
+	
+	public void setDiameter(double diameter)
+	{
+		this.diameter = diameter;
+	}
+	
+	public double getDiameter()
+	{
+		return diameter;
+	}
+	
+	public double volume(double diameter)
+	{
+		double radius = diameter / 2;
+		
+		return Math.round(4.0/3.0 * Math.PI * radius * radius * radius);
+	}
+	
+	public double surfaceArea(double diameter)
+	{
+		double radius = diameter / 2;
+		
+		return Math.round(4 * Math.PI * radius * radius);
+	}
+	
+	public String toString()
+	{
+		return "Diameter " + getDiameter() + ", Volume " + volume(getDiameter()) + ", Surface Area " + surfaceArea(getDiameter());
+	}
+	
 }
